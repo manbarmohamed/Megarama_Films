924afc39e0b52a592c2770bb25909f732115a8bf
oussama charafi <ou.charafi@gmail.com>
2024-04-23 16:04:13 +0100

    I created fucntion afficherHistorque() et fonction ajouterReservation()

--- src/main/java/com/Cenima/DAO/ReservationDAO.java
+++ src/main/java/com/Cenima/DAO/ReservationDAO.java
@@ -1,5 +1,12 @@
 package com.Cenima.DAO;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.Cenima.Classes.Reservation;
+
 public interface ReservationDAO {
+	 public void saveReservation(Reservation res) throws SQLException;
+	 public List<Reservation> historicalReservation() throws SQLException;
 
 }
