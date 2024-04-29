cdea22a40400c38fb4b36921b2538698501340be
oussama <ou.charafi@gmail.com>
2024-04-29 14:10:00 +0100

    i linked the pages

---
+++ src/main/java/com/Cenima/Servlets/DL.java
@@ -0,0 +1,31 @@
+package com.Cenima.Servlets;
+
+import com.Cenima.DAO.FilmsDAOImp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "DL", value = "/DL")
+public class DL extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        FilmsDAOImp films = new FilmsDAOImp();
+        Integer id = Integer.valueOf(request.getParameter("id"));
+        try {
+            films.deleteFilm(id);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        this.getServletContext().getRequestDispatcher("/WEB-INF/deleteMovie.jsp").forward(request , response);
+        }
+}
--- src/main/webapp/WEB-INF/deleteMovie.jsp
+++ src/main/webapp/WEB-INF/deleteMovie.jsp
@@ -41,9 +41,9 @@
         </div>
 
         <ul class="header">
-            <li><a href="#">Add</a></li>
-            <li><a href="#" id="a1">Delete</a></li>
-            <li><a href="#">Update</a></li>
+            <li><a href="/WEB-INF/addMovies.jsp" id="a1">Add</a></li>
+            <li><a href="/WEB-INF/deleteMovie.jsp">Delete</a></li>
+            <li><a href="/WEB-INF/ShowFilms.jsp">Home</a></li>
         </ul>
     </div>
 
